06c2f2b2b8b93439f4fd73be22fd13734b0af8dd
jboat92 <jboateng92@hotmail.co.uk>
2021-10-27 15:55:30 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+function plus(){
+    const number1 = parseFloat(prompt('Enter first number: '));
+  const number2 = parseFloat(prompt('Enter second number: '));
+     result = number1 + number2;
+    alert(result);
+  }
+
+
+function multiply(){
+    const number1 = parseFloat(prompt('Enter first number: '));
+  const number2 = parseFloat(prompt('Enter second number: '));
+     result = number1 * number2;
+    alert(result);
+  }
+  
+  function subtract(){
+    const number1 = parseFloat(prompt('Enter first number: '));
+  const number2 = parseFloat(prompt('Enter second number: '));
+     result = number1 - number2;
+    alert(result);
+  }
+  
+  function divide(){
+    const number1 = parseFloat(prompt('Enter first number: '));
+  const number2 = parseFloat(prompt('Enter second number: '));
+     result = number1 / number2;
+    alert(result);
+  }
+  
+  function squareroot(){
+    const number1 = parseFloat(prompt('Enter number: '));
+     result = Math.sqrt(number1);
+    alert(result);
+  }
+  
+  function power(){
+    const number1 = parseFloat(prompt('Enter first number: '));
+  const number2 = parseFloat(prompt('Enter second number: '));
+     result = Math.pow (number1,number2);
+    alert(result);
+  }
